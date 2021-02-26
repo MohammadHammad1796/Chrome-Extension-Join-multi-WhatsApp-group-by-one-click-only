2f15d5dc035f2157f902ad361beee39b55377544
Mohammad Hammad <MohammadHammad1796@gmail.com>
2021-02-26 03:27:11 +0200

    Add files via upload

--- javascript/content.js
+++ javascript/content.js
@@ -11,14 +11,47 @@ chrome.storage.local.get(["groupLinks"], function (result) {
       try {
         var element = document.getElementById("side");
         if (element) {
-          var divTags = document.getElementsByTagName("div");
-          var searchText = "الانضمام إلى المجموعة";
-          var acceptElement;
-
-          for (var i = 0; i < divTags.length; i++) {
-            if (divTags[i].textContent == searchText) {
-              acceptElement = divTags[i];
-              acceptElement.click();
+          setTimeout(() => {
+            var divTags = document.getElementsByTagName("div");
+            var searchText = "الانضمام إلى المجموعة";
+            var acceptElement;
+            var isJoined = false;
+            for (var i = 0; i < divTags.length; i++) {
+              if (divTags[i].textContent == searchText) {
+                acceptElement = divTags[i];
+                acceptElement.click();
+                clearInterval(interval);
+                interval = null;
+                urls.splice(urls.length - 1, 1);
+                chrome.storage.local.set({ groupLinks: urls });
+                isJoined = true;
+                if (urls.length > 0) {
+                  window.open(urls[urls.length - 1], "_blank");
+                  close();
+                }
+                break;
+              }
+            }
+            if (!isJoined) {
+              isJoined = "joined before";
+              searchText =
+                "تعذّر الانضمام إلى هذه المجموعة. الرجاء المحاولة مجدداً.";
+              for (var i = 0; i < divTags.length; i++) {
+                if (divTags[i].textContent == searchText) {
+                  clearInterval(interval);
+                  interval = null;
+                  urls.splice(urls.length - 1, 1);
+                  chrome.storage.local.set({ groupLinks: urls });
+                  isJoined = false;
+                  if (urls.length > 0) {
+                    window.open(urls[urls.length - 1], "_blank");
+                    close();
+                  }
+                  break;
+                }
+              }
+            }
+            if (isJoined == "joined before") {
               clearInterval(interval);
               interval = null;
               urls.splice(urls.length - 1, 1);
@@ -27,32 +60,8 @@ chrome.storage.local.get(["groupLinks"], function (result) {
                 window.open(urls[urls.length - 1], "_blank");
                 close();
               }
-              break;
             }
-          }
-          searchText =
-            "تعذّر الانضمام إلى هذه المجموعة. الرجاء المحاولة مجدداً.";
-          for (var i = 0; i < divTags.length; i++) {
-            if (divTags[i].textContent == searchText) {
-              clearInterval(interval);
-              interval = null;
-              urls.splice(urls.length - 1, 1);
-              chrome.storage.local.set({ groupLinks: urls });
-              if (urls.length > 0) {
-                window.open(urls[urls.length - 1], "_blank");
-                close();
-              }
-              break;
-            }
-          }
-          clearInterval(interval);
-          interval = null;
-          urls.splice(urls.length - 1, 1);
-          chrome.storage.local.set({ groupLinks: urls });
-          if (urls.length > 0) {
-            window.open(urls[urls.length - 1], "_blank");
-            close();
-          }
+          }, 2000);
         }
       } catch {}
     }
